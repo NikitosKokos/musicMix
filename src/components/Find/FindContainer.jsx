d35c43a199010e1f22220225ac73c42807eefd00
Nikita Tsykunov <nikitaodessafreelance@gmail.com>
2021-07-14 21:19:29 +0300

    added responcive for search

--- src/components/Find/FindContainer.jsx
+++ src/components/Find/FindContainer.jsx
@@ -24,8 +24,6 @@ const FindContainer = ({ songs, changeFavorite, match }) => {
         }
     });
 
-    console.log(findSongs);
-
     return <List
         title='Find list'
         songs={findSongs}
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,7 @@ import './style.scss';
 
 const Header = () => {
     const [searchValue, setSearchValue] = React.useState('');
+    const formRef = React.useRef(null);
     const history = useHistory();
 
     const onSearchInputChange = (e) => {
@@ -14,7 +15,16 @@ const Header = () => {
         e.preventDefault();
         if(searchValue){
             history.push(`/find/${searchValue}`);
+            hideSearch();
         }
+    }
+
+    const showSearch = () => {
+        formRef.current.classList.add('show');
+    }
+
+    const hideSearch = () => {
+        formRef.current.classList.remove('show');
     }
 
     return (
@@ -22,23 +32,44 @@ const Header = () => {
             <div className="header__content container">
                 <div className="header__main">
                     <Link to='/' className="header__logo">Music Mix</Link>
-                    <form onSubmit={searchSong} className="header__search">
-                        <input 
-                            className="header__input"
-                            type="text"
-                            placeholder='Search music'
-                            value={searchValue}
-                            onChange={onSearchInputChange}
-                        />
-                        <button className="header__btn">
-                        <svg viewBox="0 0 270 281">
-                        <circle cx="170" cy="100" r="87.5" fill='none' stroke="#0B032D" strokeWidth="25"/>
-                        <rect x="105.287" y="158" width="25" height="148.898" rx="12.5" transform="rotate(45 105.287 158)" fill="#0B032D"/>
-                        </svg>
-                        </button>
-                    </form>
+                    <div ref={formRef} className="header__search">
+                        <form onSubmit={searchSong} className="header__form">
+                            <input 
+                                className="header__input"
+                                type="text"
+                                placeholder='Search music'
+                                value={searchValue}
+                                onChange={onSearchInputChange}
+                            />
+                            <button className="header__btn">
+                            <svg viewBox="0 0 270 281">
+                            <circle cx="170" cy="100" r="87.5" fill='none' stroke="#0B032D" strokeWidth="25"/>
+                            <rect x="105.287" y="158" width="25" height="148.898" rx="12.5" transform="rotate(45 105.287 158)" fill="#0B032D"/>
+                            </svg>
+                            </button>
+                        </form>
+                        <div className="header__close"  onClick={hideSearch}>
+                            <svg viewBox="0 0 426 426" fill="none">
+                            <g clipPath="url(#clip0)">
+                            <path d="M163.503 106.934C147.882 91.313 122.555 91.313 106.934 106.934C91.313 122.555 91.313 147.882 106.934 163.503L262.497 319.066C278.118 334.687 303.445 334.687 319.066 319.066C334.687 303.445 334.687 278.118 319.066 262.497L163.503 106.934Z" fill="white"/>
+                            <path d="M106.934 262.497C91.313 278.118 91.313 303.445 106.934 319.066C122.555 334.687 147.882 334.687 163.503 319.066L319.066 163.503C334.687 147.882 334.687 122.555 319.066 106.934C303.445 91.313 278.118 91.313 262.497 106.934L106.934 262.497Z" fill="white"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0">
+                            <rect width="300" height="300" fill="white" transform="translate(0.867966 213) rotate(-45)"/>
+                            </clipPath>
+                            </defs>
+                            </svg>
+                        </div>
+                    </div>
                 </div>
                 <div className="header__buttons">
+                    <div className="header__button header__button_find" onClick={showSearch}>
+                        <svg viewBox="0 0 350 354" fill="none">
+                        <circle cx="224.787" cy="125" r="90" stroke="white" strokeWidth="70"/>
+                        <rect width="70" height="200" rx="35" transform="matrix(-0.707107 -0.707107 -0.707107 0.707107 190.919 212.497)" fill="white"/>
+                        </svg>
+                    </div>
                     <Link to='/favorites' className="header__button header__button_heart">
                         <svg viewBox="0 0 358 323" fill="none">
                         <path d="M71 4C128.2 -12.4 167.167 31.5 179.5 55.5C179.5 43.5 233 -18 293 7C341 27 355 66.6667 356 84C366.4 116.8 328 175.667 307.5 201C250.7 269 198.167 310.333 179 322.5C131.8 291.3 76.6667 232.833 55 207.5C12.2 161.1 0.833331 112.833 0.499997 94.5C0.166663 71.1667 13.8 20.4 71 4Z" fill="white"/>
